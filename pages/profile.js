ec039b45071d550fecf962d4da7829ee6869bd44
vatojuan <juanchi190_16@hotmail.com>
2025-02-05 03:01:22 -0300

    Agrega página de perfil básica

---
+++ pages/profile.js
@@ -0,0 +1,13 @@
+// pages/profile.js
+export default function Profile() {
+    return (
+      <div style={{ textAlign: "center", marginTop: "2rem" }}>
+        <h1>Perfil</h1>
+        <p>Aquí podrás actualizar tus datos.</p>
+        <p>
+          <a href="/dashboard">Volver al Dashboard</a>
+        </p>
+      </div>
+    );
+  }
+  
