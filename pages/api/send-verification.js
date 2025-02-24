ba0d5041d5a89e4fb5e4e1fd1055ff1206df8715
vatojuan <juanchi190_16@hotmail.com>
2025-02-23 22:01:28 -0300

    final5 register11111234

--- pages/api/send-verification.js
+++ pages/api/send-verification.js
@@ -10,6 +10,7 @@ export default async function handler(req, res) {
   
   // Configura el transporte SMTP usando tus credenciales de Google Workspace
   let transporter = nodemailer.createTransport({
+
     host: process.env.SMTP_HOST, 
     port: process.env.SMTP_PORT,
     secure: process.env.SMTP_PORT == 465, // true para puerto 465
@@ -21,12 +22,12 @@ export default async function handler(req, res) {
 
   try {
     await transporter.sendMail({
-      from: `"Tu Empresa" <${process.env.SMTP_USER}>`,
+      from: `"No Reply" <no-reply@fapmendoza.com>`,
       to: email,
       subject: "Código de verificación",
-      text: `Utiliza este código para verificar tu correo: ${code}`,
-      html: `<p>Utiliza este código para verificar tu correo: <strong>${code}</strong></p>`,
-    });
+      text: `Utiliza este código para verificar tu correo: ${verificationCode}`,
+      html: `<p>Utiliza este código para verificar tu correo: <strong>${verificationCode}</strong></p>`,
+    });    
     return res.status(200).json({ message: "Correo enviado correctamente" });
   } catch (error) {
     console.error("Error enviando correo:", error);
