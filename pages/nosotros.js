2e98aa359bb3e00d5b7d8711b83993403a4bc4c3
vatojuan <juanchi190_16@hotmail.com>
2025-03-21 02:16:25 -0300

    agregar nosotros12

--- pages/nosotros.js
+++ pages/nosotros.js
@@ -6,21 +6,29 @@ export default function Nosotros() {
   return (
     <Box
       sx={{
+        position: "fixed",
+        top: 0,
+        left: 0,
         width: "100vw",
         height: "100vh",
-        bgcolor: "background.default",
         overflow: "hidden",
-        position: "relative",
+        bgcolor: "background.default",
       }}
     >
-      <iframe
-        src="/nosotros.pdf#toolbar=0&navpanes=0&scrollbar=0"
-        style={{
-          width: "100%",
-          height: "100%",
-          border: "none",
-        }}
-      ></iframe>
+      <object
+        data="/nosotros.pdf#toolbar=0&navpanes=0&scrollbar=0"
+        type="application/pdf"
+        width="100%"
+        height="100%"
+        style={{ display: "block" }}
+      >
+        <p>
+          El PDF no se pudo cargar.{" "}
+          <a href="/nosotros.pdf" target="_blank" rel="noopener noreferrer">
+            Descargar PDF
+          </a>
+        </p>
+      </object>
       <Box sx={{ position: "absolute", top: 16, left: 16 }}>
         <Button component={Link} href="/" variant="contained" color="primary">
           Volver al Inicio
