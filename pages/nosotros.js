a74da1a60e3733bb7f756e8baf04fc9c16d87a3a
vatojuan <juanchi190_16@hotmail.com>
2025-03-21 02:14:25 -0300

    agregar nosotros1

--- pages/nosotros.js
+++ pages/nosotros.js
@@ -1,45 +1,31 @@
 // pages/nosotros.js
-import { Box, Container, Typography, Button } from "@mui/material";
+import { Box, Button } from "@mui/material";
 import Link from "next/link";
 
 export default function Nosotros() {
   return (
-    <Box sx={{ minHeight: "100vh", bgcolor: "background.default", py: 4 }}>
-      <Container maxWidth="md">
-        <Typography variant="h4" align="center" gutterBottom>
-          Nosotros
-        </Typography>
-
-        <Box
-          sx={{
-            position: "relative",
-            width: "100%",
-            height: "calc(100vh - 250px)",
-            border: "1px solid #ccc",
-            borderRadius: 2,
-            overflow: "hidden",
-            mb: 4,
-          }}
-        >
-          <iframe
-            src="/nosotros.pdf"
-            width="100%"
-            height="100%"
-            style={{ border: "none" }}
-          ></iframe>
-        </Box>
-
-        <Box sx={{ textAlign: "center" }}>
-          <Button
-            component={Link}
-            href="/"
-            variant="contained"
-            color="primary"
-          >
-            Volver al Inicio
-          </Button>
-        </Box>
-      </Container>
+    <Box
+      sx={{
+        width: "100vw",
+        height: "100vh",
+        bgcolor: "background.default",
+        overflow: "hidden",
+        position: "relative",
+      }}
+    >
+      <iframe
+        src="/nosotros.pdf#toolbar=0&navpanes=0&scrollbar=0"
+        style={{
+          width: "100%",
+          height: "100%",
+          border: "none",
+        }}
+      ></iframe>
+      <Box sx={{ position: "absolute", top: 16, left: 16 }}>
+        <Button component={Link} href="/" variant="contained" color="primary">
+          Volver al Inicio
+        </Button>
+      </Box>
     </Box>
   );
 }
